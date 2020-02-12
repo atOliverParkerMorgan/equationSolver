393c54a30c5acd8facb2e9d692601394bc9a9f82
Oliver <oliver.parker.morgan@gmail.com>
2020-02-12 14:16:27 +0100

    second commit

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,7 +1,10 @@
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
+// this method analyzes the user input and refactors it for further calculations
 class Analyzer {
 
     public static final String allowedOperations = "/*-+";
@@ -28,6 +31,7 @@ class Analyzer {
         for (i = 0; i<input.length() ; i++) {
             if(Character.isDigit(input.charAt(i))||input.charAt(i)=='x'){
                 current.append(input.charAt(i));
+
             }else {
                 sort.add(current.toString());
                 current = new StringBuilder();
@@ -40,10 +44,25 @@ class Analyzer {
                 sort.add(current.toString());
                 current = new StringBuilder();
             }
-
-
         }
         sort.add(current.toString());
+        for (int j = 0; j < sort.size(); j++) {
+            if(Character.isDigit(sort.get(j).charAt(0))||sort.get(j).charAt(0)=='x'){
+                if(j==0){
+                    sort.set(j, '+' + sort.get(j));
+
+                }else {
+                    if(sort.get(j-1).equals("-")||sort.get(j-1).equals("+")) {
+                        sort.set(j, sort.get(j-1) + sort.get(j));
+                        sort.remove(j-1);
+                        j--;
+                    }else {
+                        sort.set(j, "+" + sort.get(j));
+                    }
+                }
+            }
+        }
+
     }
     void rootAndSquareRoot(List<String> equationSide){
         calculatingSide(equationSide,"**");
@@ -54,28 +73,61 @@ class Analyzer {
         calculatingSide(equationSide,"/");
     }
     void addAndSubtractLogic(List<String> equationSide){
-        calculatingSide(equationSide,"+");
-        calculatingSide(equationSide,"-");
+        boolean foundFirstNumber = false;
+        String stringValueTotal = "";
+        for (int i = 0; i < equationSide.size(); i++) {
+            if(!foundFirstNumber&&Character.isDigit(equationSide.get(i).charAt(1))){
+                stringValueTotal = equationSide.get(i);
+                foundFirstNumber = true;
+                equationSide.remove(equationSide.get(i));
+                i--;
+            }else if(Character.isDigit(equationSide.get(i).charAt(1))){
+                double x = Calculator.add(stringValueTotal,equationSide.get(i));
+
+                if(x>=0){
+                    stringValueTotal = "+" + (Calculator.add(stringValueTotal,equationSide.get(i)));
+                }else{
+                    stringValueTotal = "-" + (Calculator.add(stringValueTotal,equationSide.get(i)));
+                }
+
+                equationSide.remove(equationSide.get(i));
+                i--;
+            }
+        }
+        equationSide.add(stringValueTotal);
+
     }
 
     private void calculatingSide(List<String> equationSide, String operator){
+        int end = 0;
         while(equationSide.contains(operator)) {
             for (int i = 0; i < equationSide.size(); i++) {
-                if (equationSide.get(i).equals(operator) && Character.isDigit(equationSide.get(i - 1).charAt(0)) && Character.isDigit(equationSide.get(i + 1).charAt(0))) {
+                if (equationSide.get(i).equals(operator) && Character.isDigit(equationSide.get(i - 1).charAt(1)) && Character.isDigit(equationSide.get(i + 1).charAt(1))) {
                     double x = Calculator.doAction(operator,equationSide.get(i - 1), equationSide.get(i + 1));
-                    equationSide.set(i, Double.toString(x));
+                    if(x>=0){
+                        equationSide.set(i, '+'+Double.toString(x));
+                    }else {
+                        equationSide.set(i, '-'+Double.toString(x));
+                    }
+
                     equationSide.remove(equationSide.get(i - 1));
                     equationSide.remove(equationSide.get(i));
                     break;
 
                 }
             }
+            if(end == 1000){
+                break;
+            }
+            end++;
         }
     }
 
     public void printEquation(){System.out.println( Arrays.toString(sortL.toArray())+" = "+Arrays.toString(sortR.toArray()));}
 
     private void brackets(){
+
+
     }
 
 }
--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,6 @@
 public class UI {
     public static void main(String[] args) {
-        Analyzer A = new Analyzer("7**2","12+31-3+2");
+        Analyzer A = new Analyzer("7*2","12-x+31-2");
         A.order();
         A.printEquation();
         A.rootAndSquareRoot(A.sortL);
