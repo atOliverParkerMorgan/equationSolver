3f1a77d1cd5e49a5c4e05e33c340923bba428012
Oliver <oliver.parker.morgan@gmail.com>
2020-02-26 15:14:34 +0100

    main operations finished(**, //, *, /, +, -) working on brackets

--- src/Analyzer.java
+++ src/Analyzer.java
@@ -1,8 +1,7 @@
-import java.awt.geom.Rectangle2D;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.jar.JarOutputStream;
 
 
 // this method analyzes the user input and refactors it for further calculations
@@ -26,10 +25,16 @@ class Analyzer {
     }
     public void solve(){
         order();
+
+        // left side
         printEquation();
         rootAndSquareRoot(sortL);
         multiplyAndDivideLogic(sortL);
         addAndSubtractLogic(sortL);
+
+
+        // right side
+        brackets(sortR);
         rootAndSquareRoot(sortR);
         multiplyAndDivideLogic(sortR);
         addAndSubtractLogic(sortR);
@@ -91,15 +96,46 @@ class Analyzer {
         }
 
     }
-    void rootAndSquareRoot(List<String> equationSide){
+    private void brackets(List<String> equationSide) {
+        boolean deleteNextBracket = false;
+
+        while (equationSide.contains("(") || equationSide.contains(")")) {
+            for (int i1 = 0; i1 < equationSide.size(); i1++) {
+                if (equationSide.get(i1).equals("(")) {
+                    if(i1==0){
+                        deleteNextBracket = true;
+                    }else if(equationSide.get(i1-1).equals("+")||equationSide.get(i1-1).equals("-")){
+                        deleteNextBracket = true;
+                    }
+
+                    for (int i2 = 0; i2 < equationSide.size(); i2++) {
+                        if (equationSide.get(i2).equals(")")) {
+                            if(i2==equationSide.size()-1&&deleteNextBracket){
+                                equationSide.remove(i1);
+                                equationSide.remove(i2-1);
+                                break;
+                            }else if((equationSide.get(i2+1).contains("+")||equationSide.get(i2+1).contains("-")) && deleteNextBracket){
+                                equationSide.remove(i1);
+                                equationSide.remove(i2-1);
+                                break;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+
+    private void rootAndSquareRoot(List<String> equationSide){
         calculatingSide(equationSide,"**");
         calculatingSide(equationSide,"//"); }
 
-    void multiplyAndDivideLogic(List<String> equationSide){
+    private void multiplyAndDivideLogic(List<String> equationSide){
         calculatingSide(equationSide,"*");
         calculatingSide(equationSide,"/");
     }
-    void addAndSubtractLogic(List<String> equationSide){
+    private void addAndSubtractLogic(List<String> equationSide){
         boolean foundFirstNumber = false;
         String stringValueTotal = "";
         for (int i = 0; i < equationSide.size(); i++) {
@@ -111,10 +147,19 @@ class Analyzer {
             }else if(Character.isDigit(equationSide.get(i).charAt(1))){
                 double x = Calculator.add(stringValueTotal,equationSide.get(i));
 
-                if(x>=0){
-                    stringValueTotal = "+" + (Calculator.add(stringValueTotal,equationSide.get(i)));
-                }else{
-                    stringValueTotal = "-" + (Calculator.add(stringValueTotal,equationSide.get(i)));
+                if(x>=0 ){
+                    if(!Double.toString(Calculator.add(stringValueTotal,equationSide.get(i))).contains("+")){
+                        stringValueTotal = "+"+ Calculator.add(stringValueTotal, equationSide.get(i));
+                    }else{
+                        stringValueTotal = Double.toString(Calculator.add(stringValueTotal, equationSide.get(i)));
+                    }
+                }else if(x<0){
+                    if(!Double.toString(Calculator.add(stringValueTotal,equationSide.get(i))).contains("-")){
+                        stringValueTotal = "-" + Calculator.add(stringValueTotal, equationSide.get(i));
+                    }else{
+                        stringValueTotal = Double.toString(Calculator.add(stringValueTotal, equationSide.get(i)));
+                    }
+
                 }
 
                 equationSide.remove(equationSide.get(i));
@@ -126,28 +171,32 @@ class Analyzer {
     }
 
     private void calculatingSide(List<String> equationSide, String operator){
+
         while(equationSide.contains(operator)) {
             for (int i = 0; i < equationSide.size(); i++) {
-                if (equationSide.get(i).equals(operator) && Character.isDigit(equationSide.get(i - 1).charAt(1)) && Character.isDigit(equationSide.get(i + 1).charAt(1))) {
-                    double x = Calculator.doAction(operator,equationSide.get(i - 1), equationSide.get(i + 1));
-                    if(x>=0){
-                        if(!Double.toString(x).contains("+")){
-                            equationSide.set(i, '+'+Double.toString(x));
-                        }else {
-                            equationSide.set(i, Double.toString(x));
-                        }
-                    }else{
-                        if(!Double.toString(x).contains("-")){
-                            equationSide.set(i, '-'+Double.toString(x));
-                        }else {
-                            equationSide.set(i, Double.toString(x));
+
+                if (equationSide.get(i).equals(operator) ) {
+                    if(Character.isDigit(equationSide.get(i - 1).charAt(1)) && Character.isDigit(equationSide.get(i + 1).charAt(1))) {
+                        double x = Calculator.doAction(operator, equationSide.get(i - 1), equationSide.get(i + 1));
+                        if (x >= 0) {
+                            if (!Double.toString(x).contains("+")) {
+                                equationSide.set(i, '+' + Double.toString(x));
+                            } else {
+                                equationSide.set(i, Double.toString(x));
+                            }
+                        } else {
+                            if (!Double.toString(x).contains("-")) {
+                                equationSide.set(i, '-' + Double.toString(x));
+                            } else {
+                                equationSide.set(i, Double.toString(x));
+                            }
+
                         }
 
+                        equationSide.remove(equationSide.get(i - 1));
+                        equationSide.remove(equationSide.get(i));
+                        break;
                     }
-
-                    equationSide.remove(equationSide.get(i - 1));
-                    equationSide.remove(equationSide.get(i));
-                    break;
 
                 }
             }
@@ -156,9 +205,5 @@ class Analyzer {
 
     public void printEquation(){System.out.println( Arrays.toString(sortL.toArray())+" = "+Arrays.toString(sortR.toArray()));}
 
-    private void brackets(){
-
-
-    }
 
 }
--- src/UI.java
+++ src/UI.java
@@ -1,6 +1,7 @@
 public class UI {
     public static void main(String[] args) {
-        Analyzer A = new Analyzer("-2+9","3/3");
+        Analyzer A = new Analyzer("-9+8","(3+3*-3+49**2)");
         A.solve();
+
     }
 }
